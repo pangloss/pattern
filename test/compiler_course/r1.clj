683450026d3b1d020e0fbb7b99e98ddb0ac9de7a
Darrick Wiebe <darrickw@gmail.com>
2021-05-24 13:36:13 -0400

    Slightly smarter instruction selection, make the generated code more complete

--- test/compiler_course/r1.clj
+++ test/compiler_course/r1.clj
@@ -3,6 +3,11 @@
             [matches.nanopass.dialect :refer [=> ==> ===>]]
             [matches.nanopass.pass :refer [defpass let-rulefn]]))
 
+(def niceid (atom 0))
+
+(defn gennice [sym]
+  (symbol (str (name sym) \. (swap! niceid inc))))
+
 (defpass interp-exp nil
   (let-rulefn [(Expr nil
                      [(rule '(Int ?n)
@@ -27,7 +32,7 @@
 #_ ;; FIXME: this should work but (success) fails.
 (def uniqify
   (directed (rule-list [(rule '(let ([?x ?e]) ?body)
-                              (let [x' (gensym x)
+                              (let [x' (gennice x)
                                     env (assoc-in %env [:vars x] x')]
                                 (sub (let ([?x' ~(in e env)])
                                        ~(in body env)))))
@@ -39,7 +44,7 @@
 (def uniqify
   (directed (rule-list (rule '(program ?->p) (sub (program ?p)))
                        (rule '(let ([?x ?e]) ?body)
-                             (let [x' (gensym x)
+                             (let [x' (gennice x)
                                    env (assoc-in %env [:vars x] x')]
                                (sub (let ([?x' ~(in e env)])
                                       ~(in body env)))))
@@ -63,18 +68,18 @@
                                          (:s body))
                               :value (:value body)})
                        (rule '(+ ?->a ?->b)
-                             (let [t (gensym 'tmp+)]
+                             (let [t (gennice 'tmp+)]
                                {:v (concat (:v a) (:v b) [t])
                                 :s (concat (:s a) (:s b)
                                            [(sub (assign ?t (+ ~(:value a) ~(:value b))))])
                                 :value t}))
                        (rule '(- ?->a)
-                             (let [t (gensym 'tmp-)]
+                             (let [t (gennice 'tmp-)]
                                {:v (concat (:v a) [t])
                                 :s (concat (:s a) [(sub (assign ?t (- ~(:value a))))])
                                 :value t}))
                        (rule '(read)
-                             (let [t (gensym 'read)]
+                             (let [t (gennice 'read)]
                                {:v [t]
                                 :s [(sub (assign ?t (read)))]
                                 :value t}))
@@ -104,23 +109,33 @@
   (directed (rule-list (rule '(program ?vars ??->instrs)
                              (sub (program ?vars ~@(apply concat instrs))))
                        (rule '(assign ?x (+ ?->a ?->b))
-                             (sub [(movq ?a (var ?x))
-                                   (addq ?b (var ?x))]))
+                             (let [x (sub (v ?x))]
+                               (cond (= x b) (sub [(addq ?a ?b)])
+                                     (= x a) (sub [(addq ?b ?a)])
+                                     :else (sub [(movq ?a ?x)
+                                                 (addq ?b ?x)]))))
                        (rule '(assign ?x (read))
-                             (sub [(callq read-int)
-                                   (movq (reg rax) (var ?x))]))
+                             (let [x (sub (v ?x))]
+                               (sub [(callq read-int)
+                                     (movq (reg rax) ?x)])))
                        (rule '(assign ?x (- ?->a))
-                             (sub [(movq ?a (var ?x))
-                                   (negq (var ?x))]))
+                             (let [x (sub (v ?x))]
+                               (if (= x a)
+                                 (sub [(negq ?x)])
+                                 (sub [(movq ?a ?x)
+                                       (negq ?x)]))))
                        (rule '(assign ?x ?->a)
-                             (sub [(movq ?a (var ?x))]))
+                             (let [x (sub (v ?x))]
+                               (if (= x a)
+                                 []
+                                 (sub [(movq ?a ?x)]))))
                        (rule '(return ?->x)
                              (sub [(movq ?x (reg rax))
                                    (retq)]))
                        (rule '(? i int?)
                              (sub (int ?i)))
                        (rule '(? v symbol?)
-                             (sub (var ?v))))))
+                             (sub (v ?v))))))
 
 (def sfu (comp #'select-instructions #'flatten #'uniqify))
 
@@ -145,7 +160,7 @@
                              (sub (program ~(* 16 (max 1 (int (Math/ceil (/ (:var-count %env 0) 2)))))
                                            ?vars ??i*)))
                        (on-subexpressions
-                        (rule '(var ?x)
+                        (rule '(v ?x)
                               (let [offset (get-in %env [:offset x])
                                     found offset
                                     offset (or offset (* -8 (inc (:var-count %env 0))))
@@ -209,15 +224,27 @@
 
 (def stringify
   (directed (rule-list (rule '(program ?size ?vars ??->i*)
-                             (apply str "\t.globl main\n"
-                                    "main:\n"
-                                    (map #(str "\t" % "\n") i*)))
+                             (str
+                              "start:\n"
+                              (apply str (map #(str "\t" % "\n") i*))
+                              "\tjmp conclusion\n"
+                              "\t.globl main\n"
+                              "main:\n"
+                              "\tpushq %rbp\n"
+                              "\tmovq %rsp, %rbp\n"
+                              "\tsubq $" size ", %rsq\n"
+                              "\tjmp start\n"
+                              "conclusion:\n"
+                              "\taddq $" size ", %rsp\n"
+                              "\tpopq %rbp\n"
+                              "\tretq\n"))
                        (rule '(int ?i)
                              (str "$" i))
                        (rule '(deref ?v ?o)
                              (str o "(%" (name v) ")"))
                        (rule '(reg ?r)
                              (str "%" r))
+                       (rule '(ret) "")
                        (rule '(?x)
                              (name x))
                        (rule '(?x ?->a)
