54331a5aa2fa4cbd94f602ce6f5c336b4a7d59c0
Darrick Wiebe <darrickw@gmail.com>
2022-06-22 22:28:06 -0400

    A previously uncommitted change

--- src/matches/r3/rewrite.clj
+++ src/matches/r3/rewrite.clj
@@ -433,7 +433,7 @@
   the rule macro runs. It uses the metadata to let-bind values from %env within
   rule-handlers."
   (directed
-   (rule-list (rule '((?:chain (? op symbol?) name (| "rule-list" "in-order")) (?:* (| [??rules] ?->rule)))
+   (rule-list (rule '((?:chain (? op symbol?) name (| "rule-list" "in-order" "on-subexpressions")) (?:* (| [??rules] ?->rule)))
                     ;; FIXME: why didn't [??->rules] work in this context? The
                     ;; rule should have worked with the following, without the
                     ;; descend code below:
