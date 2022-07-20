4dc7df48291dec2026bfed4fae89b208ea0f3e70
Darrick Wiebe <darrickw@gmail.com>
2022-07-20 02:19:49 -0400

    Rewrite of deep-merge-meta in progress. Better walk, I think...
    It should correctly handle diffs in the tree without bailing out on the
    first change.
    
    Possible problem is that it's too deeply recursive. I may need a way to
    limit that, or change the way I'm using it...

--- src/pattern/util.clj
+++ src/pattern/util.clj
@@ -102,7 +102,7 @@
 (defn zpos [z]
   (z 1))
 
-(defn walk-equal-subtree [oz rz f]
+(defn walk-equal-subtree [oz rz on-same]
   (let [stop (zpos (skip oz))]
     (loop [oz oz
            rz rz]
@@ -111,7 +111,9 @@
         (let [on (zip/node oz)]
           (recur (zip/next oz)
             ;; note this is identical
-            (zip/next (zip/edit rz f on #_#_with-meta (merge (meta on) (meta rn))))))))))
+            (zip/next (if on-same
+                        (on-same rz on)
+                        rz))))))))
 
 ;; example for walk-diff: ! means nodes are equal, . is no change, + is skip forward, L is old, R is new
 ;; ! ! 0 [[:+ 1 [0]] [:+ 3 [(let [a 1] (let [b 2] (+ (+ (+ (+ a 36) b) 1) 2))) 8]] [:- 5 1]] c < i, equal nodes
@@ -147,7 +149,7 @@
         (rest d)))))
 
 
-(defn walk-diff [d oz rz same changed]
+(defn walk-diff [d oz rz on-same on-changed]
   (let [op (zip/path oz)
         rp (zip/path rz)]
     (loop [c 0
@@ -160,31 +162,17 @@
             (not= rp (zip/path rz)))
         rz
         (if (< c idx)
-          (recur (inc c) d (skip oz) (walk-equal-subtree oz rz same))
+          (recur (inc c) d (skip oz) (walk-equal-subtree oz rz on-same))
           (let [d (if (= c (+ idx ec))
                     (rest d)
                     d)]
             (case side
               :r (recur (inc c) d (skip oz)
                    (if (and (zip/branch? oz) (zip/branch? rz))
-                     (walk-diff (diff (zip/node oz) (zip/node rz)) (zip/down oz) (zip/down rz) same changed)
-                     (skip rz)))
-              :+ (recur (inc c) d oz (skip rz))
-              :- (recur (inc c) d (skip oz) rz)))))
-      #_
-      (if (zero? idx)
-        (let [on (zip/node oz)
-              nn (zip/node nz)]
-          (if (= on nn)
-            ;; identical node or subtree
-            (if (zip/branch? oz)
-              (recur (skip oz) (skip nz) (walk-equal-subtree oz rz same))
-              (recur (zip/next oz) (zip/next nz)
-                ;; note this is identical
-                (zip/next (zip/edit rz with-meta (merge (meta on) (meta nn))))))
-            (if (and (zip/branch? oz) (zip/branch? nz))
-              ())
-            (if (= (type on) (type nn)))))))))
+                     (walk-diff (diff (zip/node oz) (zip/node rz)) (zip/down oz) (zip/down rz) on-same on-changed)
+                     (skip (if on-changed (on-changed rz (zip/node oz)) rz))))
+              :+ (recur (inc c) d oz (skip (if on-changed (on-changed rz nil) rz)))
+              :- (recur (inc c) d (skip oz) (if on-changed (on-changed rz oz) rz)))))))))
 
 (defn deep-merge-meta2
   "Copy meta over from the elements in the old tree to the new tree until the trees diverge"
@@ -193,59 +181,24 @@
   ([old-tree new-tree f]
    (if (and (sequential? old-tree) (sequential? new-tree))
      (let [oz (make-zipper old-tree)
-           nz (make-zipper new-tree)
-           rz nz
-           d (second (diff old-tree new-tree))]
+           rz (make-zipper new-tree)
+           on-same (fn [rz on]
+                     (if (meta? on)
+                       (zip/edit rz #(with-meta (merge (meta on) (meta %))))
+                       rz))
+           d (diff old-tree new-tree)]
        (if (seq d)
-         (zip/root (walk-diff d oz rz same nil))
-         (zip/root (walk-equal-subtree oz rz same)))))))
-
-
+         (zip/root (walk-diff d oz rz on-same nil))
+         (zip/root (walk-equal-subtree oz rz on-same)))))))
 
 (comment
   (diff [:a :b  1 2 3] [:x :y 1 2 3 4])
   ;; => [[:r 1 0] [:+ 1 0] [:+ 1 0]]
 
-
-  (let [z (make-zipper [[1 2] 3 4])]
-    (-> z zip/next
-      zip/next
-      zip/path))
-
-
-  (simple-diff
-    '(program 0 1 2 3)
-    '(program 0  9  3))
-
   (let [a '(program 0 9 2 3)
-        b '(program 0 9 9 3)]
+        b '(program 0 9 9 3)
+        a [:a :b  1 2 3]
+        b [:x :y 1 2 3 4]]
     (walk-diff (diff a b) (zip/down (make-zipper a)) (zip/down (make-zipper b))
-      (fn [b a] (prn :same :from a :to b) b)))
-
-  [[:+ 1 [0]]
-   [:+ 3 [(let [a 1] (let [b 2] (+ (+ (+ (+ a 36) b) 1) 2))) 8]]
-   [:- 5 1]]
-  ;; => [[:+ 1 [0]] [:+ 3 [42 8]] [:- 5 1]]
-  ;; => [[:+ 1 [0]] [:+ 3 [(let [a 1] (let [b 2] (+ (+ (+ (+ a 36) b) 1) 2)))]] [:- 4 1]]
-  ;; => [[:+ 1 [0]] [:+ 3 [(let [a 1] (let [b 2] (+ (+ (+ (+ a 36) b) 1) 2))) 8]] [:- 5 1]]
-
-                                        ;diffit
-  [2 [[:- 1 1]
-      [:+ 1 [(let [a 1] (let [b 2] (+ (+ (+ (+ a 36) b) 1) 2)))]]]]
-  ;; => [2 [[:- 1 1] [:+ 1 [42]]]]
-
-  ;; astar
-  [[[1 1 2] :-]
-   [[1 1 2] :-]
-   [[1 2] :r (let [b 2] (+ (+ (+ (+ a 36) b) 1) 2))]]
-
-
-  ;; quick:
-  '[[[1 1 2] :-]
-    [[1 1 2] :-]
-    [[1 2 0] :-]
-    [[1 2 0] :-]
-    [[1 2 0] :-]
-    [[1 2 0] :r let]
-    [[1 2 1] :r [b 2]]
-    [[1 2 2] :r (+ (+ (+ (+ a 36) b) 1) 2)]])
+      (fn same [z orig] (prn :same :from orig :to (zip/node z)) z)
+      (fn changed [z orig] (prn :changed :from orig :to (zip/node z)) z))))
