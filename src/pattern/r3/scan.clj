d3ea4d6ef512207d4b04a9909d2393fd855c593f
Darrick Wiebe <darrickw@gmail.com>
2023-06-10 11:44:47 -0400

    Don't pay the overhead on small inputs

--- src/pattern/r3/scan.clj
+++ src/pattern/r3/scan.clj
@@ -1,7 +1,7 @@
 (ns pattern.r3.scan
   (:require [pattern.r3.core :refer [rule name-rule rule-fn-body pattern-args raw-matches success]]
             [pattern.r3.rewrite :refer [sub spliced]]
-            [pattern.r3.combinators :refer [in-order iterated]]
+            [pattern.r3.combinators :refer [in-order iterated guard]]
             [pattern.r3.rule :refer [make-rule *post-processor*]]
             [pattern.r3.post-process :refer [raw]]
             [pattern.match.core :refer [matcher]]))
@@ -41,68 +41,119 @@
               (sub [??before ?a ??after])))
           [1 2 2 3 3 3 4 4 4 4])
          ;; => [1 2 3 4]
-         "
+
+  For lists under about 10 elements, the above naive strategy seems a little
+  faster. If you enable ^:rescan, lists under length 10 will use the naive strategy. You can change the
+  threshold number from 10 to any n you as follows:
+
+         (scan-rule x ^{:rescan true :optimize-at-count n} '[?a ?a] [a])
+
+  Setting :optimize-at-count to 0 will cause all lengths to use the optimized
+  strategy, or setting it to Long/MAX_VALUE will force all lengths to use the
+  naive strategy."
   [name pattern body]
   (when-not (:no-assert (meta pattern))
     (assert (matcher ''[??_] pattern) "Pattern must be a vector"))
-  (let [result (gensym 'result) ?result  (symbol (str "?" result))
-        before (gensym 'before) ??before (symbol (str "??" before))
-        after  (gensym 'after)  ??after  (symbol (str "??" after))
-        full-pattern (sub '[~?result [~??before ~@(second pattern) ~??after]])
-        full-handler (if (:rescan (meta pattern))
+  (let [result  (gensym 'result)  ?result   (symbol (str "?"  result))
+        before  (gensym 'before)  ??before  (symbol (str "??" before))
+        after   (gensym 'after)   ??after   (symbol (str "??" after))
+        changed (gensym 'changed) ??changed (symbol (str "??" changed))
+        remove-quote second ;; second element in (quote x) is the element itself.
+        full-pattern (sub '[~?result [~??before ~@(remove-quote pattern) ~??after]])
+        rescan? (:rescan (meta pattern))
+        full-handler (if rescan?
                        `(let [changed# ~body]
                           [(into ~result ~before)
                            (into (vec changed#) ~after)])
                        `(let [changed# ~body]
                           [(into ~result (concat ~before changed#))
-                           ~after]))]
+                           ~after]))
+        small-pattern (when rescan?
+                        (sub '[~??before ~@(remove-quote pattern) ~??after]))
+        small-handler (when rescan?
+                        `(let [~changed ~body]
+                           (sub [~??before ~??changed ~??after])))
+        ;; number of elements that we start using the optimized strategy
+        optimize-at-count (if rescan? 0 (:optimize-at-count (meta pattern) 10))]
     `(in-order
        (raw
          (rule ~(symbol (str "PRE-" name))
            '~'(? v sequential?)
-           (success [[] (vec ~'v)])))
-       (iterated
-         (name-rule '~name
-           (let [p# ~(spliced full-pattern)]
-             (make-rule p#
+           (let [v# (vec ~'v)]
+             (if (< (count v#) ~optimize-at-count)
+               v#
+               (success [[] (vec ~'v)] (assoc ~'%env ::long true))))))
+       (guard (fn [_# env#] (::long env#))
+         (in-order
+           (iterated
+             (name-rule '~name
+               (make-rule ~(spliced full-pattern)
+                 (rule-fn-body ~name
+                   ~(pattern-args full-pattern)
+                   ~(:env-args (meta pattern))
+                   ~full-handler)
+                 raw-matches
+                 *post-processor*
+                 {:may-call-success0? false
+                  :src '~full-handler
+                  :pattern-meta '~(meta pattern)})))
+           (raw
+             (rule ~(symbol (str "POST-" name))
+               '~'[?result ?remainder]
+               (if (seq ~'result)
+                 (if (seq ~'remainder)
+                   (into ~'result ~'remainder)
+                   ~'result)
+                 ~'remainder)))))
+       (guard (fn [_# env#] (not (:long env#)))
+         (iterated
+           (name-rule '~(symbol (str "SMALL-" name))
+             (make-rule ~(spliced small-pattern)
                (rule-fn-body ~name
-                 ~(pattern-args full-pattern)
+                 ~(pattern-args small-pattern)
                  ~(:env-args (meta pattern))
-                 ~full-handler)
+                 ~small-handler)
                raw-matches
                *post-processor*
                {:may-call-success0? false
-                :src '~full-handler
-                :pattern-meta '~(meta pattern)}))))
-       (raw
-         (rule ~(symbol (str "POST-" name))
-           '~'[?result ?remainder]
-           (into ~'result ~'remainder))))))
-
-
+                :src '~small-handler
+                :pattern-meta '~(meta pattern)})))))))
 
 (comment
   (def e1
-    (scan-rule minimal-example
+    (scan-rule minimal-example ^:rescan
       '[?a ?a]
       [a]))
 
-  (e1 [1 2 2 3 3 3 4 4 4 4])
+  (e1 [1 4 4 4 4])
+
   ;; => [1 2 3 3 4 4]
 
 
-  ((scan-rule minimal-example ^:rescan
-     '[?a ?a]
-     [a])
-   [1 2 2 3 3 3 4 4 4 4])
-  ;; => [1 2 3 4]
+  (let [orig (iterated
+               (rule basic-example
+                 '[??before ?a ?a ??after]
+                 (sub [??before ?a ??after])))
+        fast (scan-rule minimal-example ^{:rescan true :optimize-at-count 0}
+               '[?a ?a]
+               [a])]
 
-  ((iterated
-     (rule basic-example
-       '[??before ?a ?a ??after]
-       (sub [??before ?a ??after])))
-   [1 2 2 3 3 3 4 4 4 4])
-  ;; => [1 2 3 4]
+    (for [n (range 0 100 2)
+          :let [data (vec (take n (cycle [1 2 2 3 3 3 4 4 4 4 5 5 5 5 5 6 6 6 6 6 6 7 7 7 7 7 7 7 8 8 8 8 8 8 8 8])))]]
+      (do
+        (println (count data))
+        (println
+          (=
+            (time
+              (do (print "fast? ")
+                  (vec
+                    (for [_ (range 1000)]
+                      (fast data)))))
+            (time
+              (do (print "orig: ")
+                  (vec
+                    (for [_ (range 1000)]
+                      (orig data))))))))))
 
 
 
